0dd67f8e6abff4f8a673953224d881d2e61a0e97
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2023-09-25 19:31:07 +0530

    Lesson2

--- chakra/src/pages/Dashboard.jsx
+++ chakra/src/pages/Dashboard.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import {Container,Heading,Text,Box} from '@chakra-ui/react'
 const Dashboard = () => {
+  const boxStyles={
+    my:"20px",
+    padding:"5px",
+    color:"white",
+    fontWeight:"bold",
+    backgroundColor:"purple.400",
+    cursor:"pointer",
+    ":hover":{
+      color:"purple.400",
+      backgroundColor:"white"
+    }
+  }
   return (
-    <div>
-      Dashboard
-    </div>
+    <Container py="20px">
+        <Heading>
+           Dashboard
+        </Heading>
+        <Text marginTop="20px">This is a Chakra Text component</Text>
+        <Box my="5px" p="5px" bg="purple.400">
+          <Text color="white">
+            This is a Chakra Box component
+          </Text>
+        </Box>
+
+        <Box sx={boxStyles}> This is box styles object</Box>
+    </Container>
   );
 }
 
