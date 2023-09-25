4bb0e059e7d474f00d215649bf2b46e3fc719820
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2023-09-25 21:52:28 +0530

    Lesson3

---
+++ chakra/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Flex,Box,Text,HStack,Heading,Button,Spacer} from '@chakra-ui/react';
+const Navbar = () => {
+  return (
+    <Flex as="nav" m="1rem" justify="space-between" alignItems="center">
+        <Box>
+            <Heading as="h1">
+                Nav title
+            </Heading>
+        </Box>
+
+        <Spacer/>
+        <HStack spacing="20px">
+            <Box bg="gray.200" p="10px">
+               M
+            </Box>
+            <Text>
+                Avatar Text
+            </Text>
+            <Button colorScheme="purple">
+                Logout
+            </Button>
+        </HStack>
+    </Flex>
+  );
+}
+
+export default Navbar;
--- chakra/src/layouts/RootLayout.jsx
+++ chakra/src/layouts/RootLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom'
-
+import Navbar from '../components/Navbar';
 const RootLayout = () => {
   return (
+    <>
+    <Navbar/>
     <Outlet/>
+    </>
   );
 }
 
