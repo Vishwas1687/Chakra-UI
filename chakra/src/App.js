c1c4176b7056c98e96df3f520091088d7f922bfe
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2023-09-25 19:16:27 +0530

    Lesson1

--- chakra/src/App.js
+++ chakra/src/App.js
@@ -1,6 +1,25 @@
+import {Link, NavLink,Routes,Route,BrowserRouter} from 'react-router-dom'
+import Dashboard from './pages/Dashboard.jsx'
+import RootLayout from './layouts/RootLayout.jsx'
+import Create from './pages/Create.jsx'
+import Profile from './pages/Profile.jsx'
 function App() {
+
+
   return (
-    null
+    <>
+    <BrowserRouter>
+       <Routes>
+
+          <Route path="/" element={<RootLayout/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/create" element={<Create/>}/>
+          </Route>
+
+       </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
